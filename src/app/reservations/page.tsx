36d83fdabca074312826db4dbfccbddf0c881fdf
Khun2901 <nunnun.234@gmail.com>
2023-08-31 20:34:44 +0700

    week5

--- src/app/reservations/page.tsx
+++ src/app/reservations/page.tsx
@@ -1,7 +1,20 @@
+import LocationDateDeserve from "@/components/LocationDateReserve"
+
 export default function Reservations() {
     return (
-        <main>
-            <div>New Reservation</div>
+        <main className="w-[100%] flex flex-col items-center space-y-4">
+            <div className="text-xl font-medium">New Reservation</div>
+            <div className="w-fit space-y-2">
+                <div className="text-md text-left text-gray-600">Pick Up Date and Location</div>
+                <LocationDateDeserve/>
+                <div className="text-md text-left text-gray-600">Return Date and Location</div>
+                <LocationDateDeserve />
+            </div>
+
+            <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm">
+                Check Car Availability
+            </button>
+            
         </main>
     )
 }
